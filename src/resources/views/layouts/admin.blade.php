17fb0ba61742a8b8fff84d35a250894751bab5db
LdM-BdS <lmendess.if@gmail.com>
2021-11-23 19:57:20 -0300

    Review Traducoes

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -162,7 +162,7 @@
                         <form
                             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                             <div class="input-group">
-                                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                <input type="text" class="form-control bg-light border-0 small" placeholder="{{ __('Search for...' )}}"
                                     aria-label="Search" aria-describedby="basic-addon2">
                                 <div class="input-group-append">
                                     <button class="btn btn-primary" type="button">
@@ -187,7 +187,7 @@
                                     <form class="form-inline mr-auto w-100 navbar-search">
                                         <div class="input-group">
                                             <input type="text" class="form-control bg-light border-0 small"
-                                                placeholder="Search for..." aria-label="Search"
+                                                placeholder="{{ __('Search for...' )}}" aria-label="Search"
                                                 aria-describedby="basic-addon2">
                                             <div class="input-group-append">
                                                 <button class="btn btn-primary" type="button">
@@ -367,7 +367,7 @@
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Edson, João V., Leoanardo, Ian 2021</span>
+                            <span>Copyright &copy; Edson, João V., Leonardo, Ian 2021</span>
                         </div>
                     </div>
                 </footer>
