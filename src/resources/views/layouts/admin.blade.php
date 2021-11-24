fd6678672c2e90b61a26614945d3c88b8f41f2d7
IanMatos <iancubamatos@gmail.com>
2021-11-24 17:13:26 -0300

    Revisão footer + revisão username + remoção barra de pesquisa

--- src/resources/views/layouts/admin.blade.php
+++ src/resources/views/layouts/admin.blade.php
@@ -159,6 +159,7 @@
                         </form>
 
                         <!-- Topbar Search -->
+                        <!--
                         <form
                             class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
                             <div class="input-group">
@@ -171,7 +172,7 @@
                                 </div>
                             </div>
                         </form>
-
+                        -->
                         <!-- Topbar Navbar -->
                         <ul class="navbar-nav ml-auto">
 
@@ -322,7 +323,7 @@
                             <li class="nav-item dropdown no-arrow">
                                 <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <!-- <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span> -->
+                                    <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                                     <img class="img-profile rounded-circle"
                                         src="{{ asset('img/undraw_profile.svg') }}">
                                 </a>
