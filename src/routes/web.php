bd73889c023c10d661c92475fa6e06c4c21ee9ab
Edson Junior <juninho.tab3@gmail.com>
2021-12-02 23:11:46 -0300

    feat: middleware de Role

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,25 +1,29 @@
 <?php
 
-use App\Http\Controllers\UsuariosController;
-use App\Http\Controllers\EventoController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PaginaInicialController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\Role;
 
 Auth::routes();
 
 Route::namespace(App\Http\Controllers::class)->group(function () {
 
-    Route::get('/', [PaginaInicialController::class, 'index'])->name('list.eventos');
-    Route::get('/home', [HomeController::class, 'index'])->name('home');
-    Route::get('/eventos', 'EventoController')->name('eventos');
-    Route::get('/eventos/novo', 'EventoController@new')->name('eventos.novo');
-    Route::post('/eventos/novo', 'EventoController@add')->name('eventos.novo');
-    Route::get('/eventos/{id}/editar', 'EventoController@edit')->name('eventos.editar');
-    Route::post('/eventos/{id}/editar', 'EventoController@update')->name('eventos.editar');
-    Route::post('/eventos/deletar', 'EventoController@delete')->name('eventos.deletar');
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/home', 'HomeController@index')->name('home');
+
+
+    Route::prefix('/eventos')->middleware('role:administrator')->group(function () {
+        Route::get('/', 'EventoController')->name('eventos');
+        Route::get('/novo', 'EventoController@new')->name('eventos.novo');
+        Route::post('/novo', 'EventoController@add')->name('eventos.novo');
+        Route::get('/{id}/editar', 'EventoController@edit')->name('eventos.editar');
+        Route::post('/{id}/editar', 'EventoController@update')->name('eventos.editar');
+        Route::post('/deletar', 'EventoController@delete')->name('eventos.deletar');
+    });
     //Route::view('/usuarios', 'usuarios.list');
+
+
     Route::get('/usuarios', 'UsuariosController@index')->name('usuarios');
     Route::get('/usuarios/{id}/editar', 'UsuariosController@edit')->name('usuarios.editar');
     Route::post('/usuarios/{id}/editar', 'UsuariosController@update')->name('usuarios.editar');
